8df8044ba6c1cb06670880295688d85901b15a46
Glitch (glitch-hello-react) <none>
2023-02-09 01:01:01 +0000

    😹⚽️ Checkpoint ./package.json:38511598/1859 ./src/components/header.jsx:38511598/18

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -62,9 +62,9 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/tokens" className="nav-link">Tokens</Link>
-            <Link to="/config" className="nav-link">Config</Link>
+            <Link href="/" className="nav-link">Home</Link>
+            <Link href="/tokens" className="nav-link">Tokens</Link>
+            <Link href="/config" className="nav-link">Config</Link>
           </Nav>
           <Nav>
             <User />
