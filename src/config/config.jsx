ebd4e77e5f86244d87459608eb2ca050dc9f0a61
Glitch (glitch-hello-react) <none>
2023-02-13 04:46:39 +0000

    👐🔖 Checkpoint ./src/config/config.jsx:38511598/9

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -11,7 +11,7 @@ const Config = {
     authority: "https://auth.pingone.asia/21be67a7-c745-4934-b6a4-6a35c918ce6a/as",
     client_id: "96024a44-b9a8-4d1b-9757-9459397c7fdb",
     redirect_uri: "https://apac-spa.glitch.me/",
-    scope: "openid profile",
+    scope: "openid profile newscope",
     skipUserInfo: true,
     onSigninCallback: onSigninCallback
   },
