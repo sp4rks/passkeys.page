d6dcfd278b6cda57f8d2c75965ca2042aabf03c5
Glitch (glitch-hello-react) <none>
2023-10-12 10:38:54 +0000

    🚇🐫 Checkpoint ./src/config/config.jsx:38511598/28

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -17,7 +17,7 @@ const Config = {
   },
   logoUrl: "https://cdn.glitch.global/b2c32b66-c81e-44db-add1-e0ac2a743e65/PIC-Horizontal-Logo-White.png?v=1697089517698",
   envId: "7bfecfd9-e298-40e1-acfd-17c534439272",
-  projectName: "apac-spa"
+  projectName: "royal-oceanic-cookie"
 };
 
 export default Config;
