e3d753fc83a52349d2c4711d2a7dd6d1db53edff
Glitch (glitch-hello-react) <none>
2023-10-15 19:18:10 +0000

    🌪💽 Checkpoint ./src/styles/styles.css:38511598/360 ./src/pages/tokens.jsx:38511598/9091

--- src/pages/tokens.jsx
+++ src/pages/tokens.jsx
@@ -1,9 +1,12 @@
 import * as React from "react";
+import { useState } from 'react';
+
 
 import { useAuth } from "react-oidc-context";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from 'react-bootstrap/Button';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 import jwt_decode from "jwt-decode";
 
@@ -13,6 +16,7 @@ import ErrorBox from "../components/error.jsx";
 function TokenPage() {
   
   const auth = useAuth();
+  const [key, setKey] = useState('idToken');
 
   function signon() {
     auth.signinRedirect();
@@ -29,7 +33,7 @@ function TokenPage() {
 
         <Col sm={5}>
           <div className="pHighlight">
-            <h1>Tokens</h1>
+            <h1>OIDC Authentication</h1>
             <p>
               This site uses OIDC to perform user authentication, and facilitate access to the PingOne device management APIs.
             </p>
@@ -45,7 +49,29 @@ function TokenPage() {
         </Col>
 
         <Col sm={7}>
-          
+          <div className="pLight pRight">
+
+            <Row sm={12}>
+              <h1>Tokens</h1>
+                <Tabs
+                id="tokenTabs"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}
+                className="mb-3"
+                >
+                <Tab eventKey="home" title="Home">
+                Tab content for Home
+                </Tab>
+                <Tab eventKey="profile" title="Profile">
+                Tab content for Profile
+                </Tab>
+                <Tab eventKey="contact" title="Contact" disabled>
+                Tab content for Contact
+                </Tab>
+                </Tabs>
+            </Row>
+
+          </div>
         </Col>
 
       </Row>
